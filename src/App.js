0a859c9058dd73d240426afe84840505e4e8daf2
Sha-MG <morgane.gamory@gmail.com>
2023-02-21 15:56:55 +0100

    Adding browser router & animal profil page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import Header from './component/Header';
 import Footer from './component/Footer';
-import './styles/index.scss';
 import './styles/reset.scss';
+import './styles/index.scss';
 import Trombinoscope from './component/Trombinoscope';
+import AnimalProfil from './component/AnimalProfil'
+import { Routes, Route} from "react-router-dom";
+
 
 function App() {
   return (
     <>
       <Header/>
-      <Trombinoscope/>
+      <Routes>
+        <Route path='/trombinoscope' element={<Trombinoscope/>} />
+        <Route path='/trombinoscope/:id' element={<AnimalProfil/>}/>
+      </Routes>
       <Footer/>
     </>
   );

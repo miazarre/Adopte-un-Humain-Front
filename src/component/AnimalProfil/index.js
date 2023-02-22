578fa5a94e38fd426ff7d84efe1625cef3de852c
Sha-MG <morgane.gamory@gmail.com>
2023-02-22 16:37:12 +0100

    Adding clicking state variation & contact form

--- src/component/AnimalProfil/index.js
+++ src/component/AnimalProfil/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from "react-router-dom"
 import { Link } from "react-router-dom"
 import data from '../../data/fake_animals.json'
 import './styles.scss'
 import {TiArrowBack} from 'react-icons/ti'
+import {RxCrossCircled} from 'react-icons/rx'
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -13,6 +14,7 @@ const AnimalProfil = () => {
     const param = useParams()
     const animal = data.filter(animal => parseInt(animal.id) === parseInt(param.id))
 
+    const [isContactingAnimal, setIsContactinganimal] = useState(false)
     const settings = {
         dots: true,
         slidesToShow: 1,
@@ -44,11 +46,38 @@ const AnimalProfil = () => {
                 <span className='animal-profil__title-container--dot'></span>
                 <p className='animal-profil__title-container--points'>10 points communs</p>
             </div>
-            <div className='animal-profil__description--text'>
-                <p>{animal[0].description}</p>
-                <p>{animal[0].description}</p>
-            </div>
-            <Link to='/trombinoscope' className='animal-profil__description--button'><span>Écrire à {animal[0].name}</span></Link>
+{/* Affichage conditionnel. Si tu cliques sur contacter, tu affiches le formulaire de contact. Si non, tu as la description de l'animal */}
+                {isContactingAnimal
+                ? <>
+                <div className='animal-profil__contact'>
+                    <div className='animal-profil__contact--container'>
+                        <textarea className='animal-profil__contact--textarea' rows="2" cols="70" placeholder='Première partie'></textarea>
+                        <p>Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi.</p>
+                    </div>
+                    <div className='animal-profil__contact--container'>
+                        <textarea className='animal-profil__contact--textarea' rows="6" cols="70" placeholder='Deuxième partie'></textarea>
+                        <p>Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi.</p>
+                    </div>
+                    <div className='animal-profil__contact--container'>
+                        <textarea className='animal-profil__contact--textarea' rows="6" cols="70" placeholder='Troisième partie'></textarea>
+                        <p>Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi.</p>
+                    </div>
+                    
+                    <div className='animal-profil__button--container'>
+                        <p className='animal-profil__button' onClick={e=>setIsContactinganimal(!isContactingAnimal)}><span>Envoyer</span></p>
+                        <p className='animal-profil__button--cross' onClick={e=>setIsContactinganimal(!isContactingAnimal)}><span>X</span></p>
+                    </div>
+                </div>
+                 </>
+                : <>
+                    <div className='animal-profil__description--text'>
+                        <p>{animal[0].description}</p>
+                        <p>{animal[0].description}</p>
+                    </div>
+                    <p className='animal-profil__description--button' onClick={e=>setIsContactinganimal(!isContactingAnimal)}><span>Écrire à {animal[0].name}</span></p>
+                </>
+                }
+            
         </div>
     </div>
     </>

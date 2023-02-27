f5292b27bdf8e14bb8757433aaead34bea247c4d
miazarre <mia.g.dev@gmail.com>
2023-02-27 15:05:45 +0100

    update Home with Axios

--- src/component/Home/index.js
+++ src/component/Home/index.js
@@ -3,7 +3,6 @@ import './styles.scss'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import axios from 'axios';
-import AnimalFav from '../Favorites/AnimalFav';
 
 const baseUrl="http://matthieuskrzypczak-server.eddi.cloud:8080/api"
 
@@ -12,7 +11,7 @@ const Home = () => {
 
     const filteredProfiles = async () => {
         const response = await axios.get(`${baseUrl}/animals`);
-        setProfiles(response.data.slice(0, 4))
+        setProfiles(response.data)
     }
      useEffect(() => {
         filteredProfiles()
@@ -31,10 +30,16 @@ const Home = () => {
                     </p> 
                 <h3>Alors, qu'attendez-vous ? Venez rencontrer votre futur ami dès aujourd'hui !</h3>
             </div>
-        <div className='animal_profiles'>
+        <div className='animal-profiles'>
             <div className='profiles'>
                 {profiles.map((profile) => (
-                    <AnimalFav animal={profile}/>
+                    <div key={profile.name} className='profile'>
+                        <div 
+                            style={{backgroundImage:`url(http://matthieuskrzypczak-server.eddi.cloud:8080/api/images/animal/${profile.photo1})`}} 
+                            className='animal-card__card--image'>
+                        </div>
+                        <h3 className='name'>{profile.name}</h3>
+                    </div>
                 ))}
                 </div>
         </div>

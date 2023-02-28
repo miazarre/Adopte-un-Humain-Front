a642d22e8936f18537a6ef8e0b1e282064445597
florentMagat <adressemail22000@gmail.com>
2023-02-28 13:30:09 +0100

    delete animal

--- src/component/AdoptionsDetail/index.js
+++ src/component/AdoptionsDetail/index.js
@@ -1,51 +1,23 @@
+import PropTypes from 'prop-types';
 import './styles.scss';
-import data from '../../data/fake_animals.json';
 
-import AnimalDetail from './AnimalDetail';
-import AnimalTable from './AnimalTable';
-
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
 import React from 'react';
 
-const AdoptionsDetail = () => {
-    const idAnimal = useParams();
-    const [animals, setAnimals] = useState([])
-    console.log(idAnimal);
-    const animalSelected= animals.filter(x => x.id === 2);
-    console.log(animalSelected);
-
-    useEffect(() => {
-        setAnimals(data)
-        }, 
-      [animals]);
-
-    const animal = animalSelected.map((animal) => (
-        <AnimalDetail
-        key={animal.id}
-        animal={animal}
-        />
-    ))
-
-    const animalTable = animalSelected.map((animal) => (
-        <AnimalTable
-        key={animal.id}
-        animal={animal}
-        />
-    ))
-
+const AdoptionsDetail = ({animal_id, form1, form2, form3}) => {
+    console.log(animal_id)
     return(
-        <div className='adoptionsdetail'>
-            <div className='adoptionsdetail__container'>
-                <div className='adoptionsdetail__container__detail'>
-                    {animal}
-                </div>
-                <div className='adoptionsdetail__container__detail-table'>
-                    {animalTable}
-                </div> 
-            </div>
-        </div>
+        <>
+            <p>{form1}</p>
+            
+        </>
     )
 }
+
+AdoptionsDetail.propTypes = {
+    animal_id: PropTypes.number.isRequired,
+    form1: PropTypes.string.isRequired,
+    form2: PropTypes.string.isRequired,
+    form3: PropTypes.string.isRequired,
+  };
 
 export default AdoptionsDetail ;

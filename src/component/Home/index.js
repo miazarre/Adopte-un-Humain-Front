d81f22b1d7e49cbe522ea9feb79cf4fefc6a612b
florentMagat <adressemail22000@gmail.com>
2023-03-01 14:09:36 +0100

    rebase

--- src/component/Home/index.js
+++ src/component/Home/index.js
@@ -3,14 +3,8 @@ import './styles.scss'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import axios from 'axios';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import AnimalFav from '../Favorites/AnimalFav';
-=======
->>>>>>> update Home with Axios
-=======
-import AnimalFav from '../Favorites/AnimalFav';
->>>>>>> add server on home
+
 
 const baseUrl="http://matthieuskrzypczak-server.eddi.cloud:8080/api"
 
@@ -19,15 +13,13 @@ const Home = () => {
 
     const filteredProfiles = async () => {
         const response = await axios.get(`${baseUrl}/animals`);
-<<<<<<< HEAD
-<<<<<<< HEAD
+
         setProfiles(response.data.slice(0, 4))
-=======
+
         setProfiles(response.data)
->>>>>>> update Home with Axios
-=======
+
         setProfiles(response.data.slice(0, 4))
->>>>>>> add server on home
+
     }
      useEffect(() => {
         filteredProfiles()
@@ -46,30 +38,10 @@ const Home = () => {
                     </p> 
                 <h3>Alors, qu'attendez-vous ? Venez rencontrer votre futur ami dès aujourd'hui !</h3>
             </div>
-<<<<<<< HEAD
-<<<<<<< HEAD
         <div className='animal_profiles'>
             <div className='profiles'>
                 {profiles.map((profile) => (
                     <AnimalFav animal={profile}/>
-=======
-        <div className='animal-profiles'>
-            <div className='profiles'>
-                {profiles.map((profile) => (
-                    <div key={profile.name} className='profile'>
-                        <div 
-                            style={{backgroundImage:`url(http://matthieuskrzypczak-server.eddi.cloud:8080/api/images/animal/${profile.photo1})`}} 
-                            className='animal-card__card--image'>
-                        </div>
-                        <h3 className='name'>{profile.name}</h3>
-                    </div>
->>>>>>> update Home with Axios
-=======
-        <div className='animal_profiles'>
-            <div className='profiles'>
-                {profiles.map((profile) => (
-                    <AnimalFav animal={profile}/>
->>>>>>> add server on home
                 ))}
                 </div>
         </div>

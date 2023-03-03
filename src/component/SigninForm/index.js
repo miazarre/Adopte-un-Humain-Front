22a88b76fcf8ec8d2366c537f8520dc2d4ca8597
Mia <113382399+miazarre@users.noreply.github.com>
2023-03-03 11:11:59 +0100

    update (#37)

--- src/component/SigninForm/index.js
+++ src/component/SigninForm/index.js
@@ -7,13 +7,17 @@ import { useState } from 'react';
 import { Navigate } from 'react-router-dom';
 import axios from 'axios';
 
+const token = localStorage.getItem('token');
+const newToken = JSON.parse(token);
+
+const baseUrl=process.env.REACT_APP_BASE_URL;
+
 const categories = [
   {name: "Licorne", image: Licorne, description:"Il/Elle aime galoper à travers les champs ouverts et les forêts, explorer de nouveaux endroits et rencontrer de nouvelles créatures. Il/elle aime aussi utiliser sa créativité pour forger de merveilleux souvenirs. Pendant notre temps libre, il/elle aime se poser, méditer et mettre en pratique ses acquis. Dans l'ensemble, il/elle est un ami et un compagnon merveilleux, toujours prêt à égayer la journée de n'importe qui avec sa nature enjouée et aimante."},
   {name: "Dinosaure", image: Dinosaure, description:"Il/elle est courageux et déterminé, toujours prêt à protéger ses proches et à défendre ses convictions. Il/elle est incroyablement loyal et affectueux envers ceux en qui il a confiance, mais il/elle peut aussi être féroce et intimidant pour les étrangers. Il/elle est aussi incroyablement curieux et adore explorer son environnement, toujours à la recherche de nouvelles images et de nouveaux sons. Il/elle aime chasser pour jouer, explorer de nouveaux territoires et faire de longues promenades dans la nature. Il/elle aime aussi passer du temps avec sa famille et ses amis, et on le trouve souvent en train de jouer avec d'autres dinosaures. Pendant son temps libre, il/elle aime se prélasser au soleil et faire de longues siestes. Dans l'ensemble, il/elle est un compagnon loyal et féroce, toujours prêt à défendre ses proches et à explorer le monde qui l'entoure."},
   {name: "Dragon", image: Dragon, description: "Il/elle est très fier et confiant, toujours avide d'aventures et de nouveaux défis. Il/elle est incroyablement intelligent et ingénieux, capable de réfléchir rapidement et de trouver des solutions créatives aux problèmes. Il/elle peut parfois être un peu têtu, mais il/elle est aussi farouchement loyal envers ceux qu'il/elle considère comme ses amis. Il/elle aime courir, jouer et amasser des trésors. Il/elle aime aussi utiliser son intelligence pour résoudre des puzzles et des énigmes, et défie souvent les autres dans des épreuves d'esprit et d'habileté. Pendant son temps libre, il/elle aime se prélasser au soleil et aiguiser ses griffes. Dans l'ensemble, il/elle est un compagnon redoutable et formidable, toujours prêt à aider ses amis ou dans une quête audacieuse."},
 ];
 
-const baseUrl="http://matthieuskrzypczak-server.eddi.cloud:8080/api";
 const SigninForm = () => {
 
   const [lastname, setLastname] = useState('');
@@ -71,16 +75,16 @@ const SigninForm = () => {
       "password": password,
       "phone": phone
   }
-
-  // Ici tu dois faire les vérifications dont j'ai parlé avant l'appelle à l'API
     console.log(newUser)
     try { 
 
-      const response  = await axios.post(`${baseUrl}/register`, 
-        newUser
+      const response  = await axios.post(`${baseUrl}/register`,
+      { headers: { Authorization: `Bearer ${newToken}` } },
+      newUser
         )
+      Navigate('/login')
 
-        console.log(response.request.statusText)
+      console.log(response.request.statusText)
 
     } catch(error) {
       console.log(error.response.data.error)
@@ -104,7 +108,7 @@ const SigninForm = () => {
 
 
         <div className="categories">
-          {/* {categories.map(c => (
+        {/*{{categories.map(c => (
             <div className="category" key={c.name.toLowerCase()} onClick={() => setCategory(c.name.toLowerCase())}>
               <div className="category-image">
               <img src={c.image} alt={c.name}/>
@@ -116,7 +120,7 @@ const SigninForm = () => {
               <p>{c.description}
               </p>
             </div>
-          ))} */}
+          ))}*/}
         </div>
 
         <p className='validation' onClick={handleSubmit}><span>Valider</span></p>

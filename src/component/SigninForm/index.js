c1bf04aac79f2767c5b1d0fdaa7de7f56abd0a8a
miazarre <mia.g.dev@gmail.com>
2023-03-07 09:37:33 +0100

    fix css

--- src/component/SigninForm/index.js
+++ src/component/SigninForm/index.js
@@ -108,7 +108,7 @@ const SigninForm = () => {
 
 
         <div className="categories">
-        {/*{{categories.map(c => (
+        {categories.map(c => (
             <div className="category" key={c.name.toLowerCase()} onClick={() => setCategory(c.name.toLowerCase())}>
               <div className="category-image">
               <img src={c.image} alt={c.name}/>
@@ -120,7 +120,7 @@ const SigninForm = () => {
               <p>{c.description}
               </p>
             </div>
-          ))}*/}
+          ))}
         </div>
 
         <p className='validation' onClick={handleSubmit}><span>Valider</span></p>

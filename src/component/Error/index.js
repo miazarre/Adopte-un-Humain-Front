72400473fb5dfd145e3589425abcc255c2c96aaa
Sha-MG <morgane.gamory@gmail.com>
2023-03-10 14:53:43 +0100

    Adding 404

--- src/component/Error/index.js
+++ src/component/Error/index.js
@@ -1,26 +1,22 @@
 import './styles.scss';
 import { Link } from 'react-router-dom';
-
+import {Player} from "@lottiefiles/react-lottie-player"
 
 //import Error from '../../../assets/Cookie.png';
 
 
 const anError = () => {
     return(
-        <> 
-            <body className="error404">
-                <p className="error404-text">
-                    <p>ERROR 404</p>
-                    <Link to="/">
-                        <button className="adoptions_container--linkToBoard error404-text-board">Retour à l'accueil</button>
-                    </Link>
-                </p>
+        <div className="error404"> 
+                <p className="error404--back"><span>Retour</span></p>
+               <Player
+               className="error404--player"
+               autoplay
+               loop
+               src="https://assets9.lottiefiles.com/packages/lf20_hd8ztfyp.json"
+               />
 
-
-                ////<img src={Error} alt="error 404" className="error404-image" />
-
-            </body>
-        </>
+        </div>
     )
 }
 

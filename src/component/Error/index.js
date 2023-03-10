247f94ee8e94e43d3ba6f8b82ddfada8134ee6b0
Sha-MG <morgane.gamory@gmail.com>
2023-03-10 14:57:25 +0100

    Adding back button 404

--- src/component/Error/index.js
+++ src/component/Error/index.js
@@ -1,14 +1,19 @@
 import './styles.scss';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import {Player} from "@lottiefiles/react-lottie-player"
 
 //import Error from '../../../assets/Cookie.png';
 
 
-const anError = () => {
+const AnError = () => {
+    const navigate = useNavigate();
+    const back = () => {
+        navigate(-1)
+    }
+
     return(
         <div className="error404"> 
-                <p className="error404--back"><span>Retour</span></p>
+                <p className="error404--back" onClick={back}><span>Retour</span></p>
                <Player
                className="error404--player"
                autoplay
@@ -20,4 +25,4 @@ const anError = () => {
     )
 }
 
-export default anError;
+export default AnError;

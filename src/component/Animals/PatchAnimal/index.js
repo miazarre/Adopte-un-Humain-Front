d066a3e6fd3b9389f69f04a71040a2a952294d0c
florentMagat <adressemail22000@gmail.com>
2023-03-12 13:44:12 +0100

    correction patch tag animal

--- src/component/Animals/PatchAnimal/index.js
+++ src/component/Animals/PatchAnimal/index.js
@@ -178,106 +178,105 @@ const addingTag = async (selectedOption) => {
           setMessage('Il y a eu une erreur.')        }
   }
 
-    return( 
-      <>
-      <Link to="/board">
-          <button className='adoptions_container--linkToBoard'>Retour au Tableau de Bord</button>
-      </Link>
-       <div className="addAnimal-container">
-          {isLogged
-          ?<><form>
-              <div className="addAnimal-container-informations">
-                  <div>
-                  <input type="text" placeholder={data.name} name="name" value={name} onChange={(event) => setName(event.target.value)} />
-                  </div>
-                  <div>
-                  <label id="date">Date de naissance :</label>
-                  <input 
-                      type="date"  
-                      name="date" 
-                      for="date" 
-                      className="informations-date" 
-                      value={birthdate} 
-                      onChange={(event) => setBirthdate(event.target.value)}/>
-                  </div>
-                  <div>
-                  <label id="photos" >Ajouter des photos :</label>
-                  <input 
-                      className="input-label" 
-                      for="photos" 
-                      onChange={(event) => setPhoto1(event.target.files[0])}
-                      type="file"
-                      name="photos"
-                      accept="image/png, image/jpeg" >
-                  </input>
-                  <input 
-                      className="input-label" 
-                      for="photos" 
-                      type="file"
-                      onChange={(event) => setPhoto2(event.target.files[0])}
-                      name="photos"
-                      accept="image/png, image/jpeg" >
-                  </input>
-                  <input 
-                      className="input-label" 
-                      for="photos"
-                      onChange={(event) => setPhoto3(event.target.files[0])}
-                      type="file"
-                      name="photos"
-                      accept="image/png, image/jpeg" >
-                  </input>
-                  <input 
-                      className="input-label" 
-                      for="photos"
-                      onChange={(event) => setPhoto4(event.target.files[0])}
-                      type="file"
-                      name="photos"
-                      accept="image/png, image/jpeg" >
-                  </input>
-                  </div>
-              </div>
-              <div className='textareas'>
-                  <div>
-                      <label for="resume" className='textareas-label'>Résumé :</label>
-                      <textarea id="resume" name="resume" rows="6" cols="45" placeholder={data.resume} value={resume} onChange={(event) => setResume(event.target.value)}></textarea>
-                  </div>
-                  <div>
-                      <label for="description" className='textareas-label'>Description :</label>
-                      <textarea id="description" rows="6" cols="45" placeholder={data.description} value={description} onChange={(event) => setDescription(event.target.value)}></textarea>
-                  </div>
-                  <div>      
-                      <label for="needs" className='textareas-label'>Besoins de l'animal :</label>
-                      <textarea id="needs" name="needs" rows="6" cols="45" placeholder={data.needs} value={needs} onChange={(event) => setNeeds(event.target.value)}></textarea>
-                  </div>   
-              </div>
-              {/* <div className="categories">
-              {/* <div className="categories">
-                  {categories.map(c => (
-                    <div className="category" onClick={() => setCategory(c.name.toLowerCase())}>
-                     <div className="category-image">
-                      <img src={c.image} alt={c.name}/>
-                      </div>
-                      <div className="category-input">
-                        <input type="radio" id={c.name.toLowerCase()} name="category" value={c.name.toLowerCase()} onChange={e => setCategory(e.target.value)} checked={c.name.toLowerCase() === category} />
-                        <label for={c.name.toLowerCase()}>{c.name}</label>
-                      </div>
-                      <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mattis ornare libero. Donec lacinia mollis massa, id hendrerit arcu faucibus et.
-                        Aliquam pretium, mi sed imperdiet maximus, lacus erat vehicula ex, vitae interdum nibh arcu a mi. 
-                        Cras vehicula, nunc mollis facilisis porta, ligula diam cursus felis, efficitur mollis tortor turpis in justo. In hac habitasse platea dictumst.
-                      </p>
+    return(
+    <>
+    <Link to="/board">
+        <button className='adoptions_container--linkToBoard'>Retour au Tableau de Bord</button>
+    </Link>
+     <div className="addAnimal-container">
+        {isLogged
+        ?<><form>
+            <div className="addAnimal-container-informations">
+                <div>
+                <input type="text" placeholder={data.name} name="name" value={name} onChange={(event) => setName(event.target.value)} />
+                </div>
+                <div>
+                <label id="date">Date de naissance :</label>
+                <input 
+                    type="date"  
+                    name="date" 
+                    for="date" 
+                    className="informations-date" 
+                    placeholder={data.birthdate}
+                    value={birthdate} 
+                    onChange={(event) => setBirthdate(event.target.value)}/>
+                </div>
+                <div>
+                <label id="photos" >Ajouter des photos :</label>
+                <input 
+                    className="input-label" 
+                    for="photos" 
+                    onChange={(event) => setPhoto1(event.target.files[0])}
+                    type="file"
+                    name="photos"
+                    accept="image/png, image/jpeg" >
+                </input>
+                <input 
+                    className="input-label" 
+                    for="photos" 
+                    type="file"
+                    onChange={(event) => setPhoto2(event.target.files[0])}
+                    name="photos"
+                    accept="image/png, image/jpeg" >
+                </input>
+                <input 
+                    className="input-label" 
+                    for="photos"
+                    onChange={(event) => setPhoto3(event.target.files[0])}
+                    type="file"
+                    name="photos"
+                    accept="image/png, image/jpeg" >
+                </input>
+                <input 
+                    className="input-label" 
+                    for="photos"
+                    onChange={(event) => setPhoto4(event.target.files[0])}
+                    type="file"
+                    name="photos"
+                    accept="image/png, image/jpeg" >
+                </input>
+                </div>
+            </div>
+            <div className='textareas'>
+                <div>
+                    <label for="resume" className='textareas-label'>Résumé :</label>
+                    <textarea id="resume" name="resume" rows="6" cols="45" placeholder={data.resume} value={resume} onChange={(event) => setResume(event.target.value)}></textarea>
+                </div>
+                <div>
+                    <label for="description" className='textareas-label'>Description :</label>
+                    <textarea id="description" rows="6" cols="45" placeholder={data.description} value={description} onChange={(event) => setDescription(event.target.value)}></textarea>
+                </div>
+                <div>      
+                    <label for="needs" className='textareas-label'>Besoins de l'animal :</label>
+                    <textarea id="needs" name="needs" rows="6" cols="45" placeholder={data.needs} value={needs} onChange={(event) => setNeeds(event.target.value)}></textarea>
+                </div>   
+            </div>
+            {/* <div className="categories">
+            {/* <div className="categories">
+                {categories.map(c => (
+                  <div className="category" onClick={() => setCategory(c.name.toLowerCase())}>
+                   <div className="category-image">
+                    <img src={c.image} alt={c.name}/>
                     </div>
-                  ))}
-                  </div> */}
-              <button onClick={handleSubmit}>Valider</button>
-              </form>
-              
-              </>
+                    <div className="category-input">
+                      <input type="radio" id={c.name.toLowerCase()} name="category" value={c.name.toLowerCase()} onChange={e => setCategory(e.target.value)} checked={c.name.toLowerCase() === category} />
+                      <label for={c.name.toLowerCase()}>{c.name}</label>
+                    </div>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mattis ornare libero. Donec lacinia mollis massa, id hendrerit arcu faucibus et.
+                      Aliquam pretium, mi sed imperdiet maximus, lacus erat vehicula ex, vitae interdum nibh arcu a mi. 
+                      Cras vehicula, nunc mollis facilisis porta, ligula diam cursus felis, efficitur mollis tortor turpis in justo. In hac habitasse platea dictumst.
+                    </p>
+                  </div>
+                ))}
+                </div> */}
+            <button onClick={handleSubmit}>Valider</button>
+            </form>
+            </>
               : <p className='connexion-message'> Il faut te connecter pour voir cette page. <Link to='/login'><p className='connexion-message--boutton'><span>Connexion</span></p></Link></p>
-  
+
               }
       </div>
-      </>,
     
     <div className='preference__page-container'>
         {isLogged
@@ -333,7 +332,8 @@ const addingTag = async (selectedOption) => {
                     
             }
         </div>
-  )
+        </>
+    )
 }
 
 PatchAnimal.propTypes = {

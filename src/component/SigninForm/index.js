3b39e12f4c395b79cbf6ed802b56b589d651f1f6
miazarre <mia.g.dev@gmail.com>
2023-10-16 14:51:41 +0200

    fix issues with Signin form

--- src/component/SigninForm/index.js
+++ src/component/SigninForm/index.js
@@ -2,9 +2,9 @@
 
 import React from "react";
 import "./styles.scss";
-// import Licorne from '../../assets/Licorne.png';
-// import Dinosaure from '../../assets/Dinosaure.png';
-// import Dragon from '../../assets/Dragon.png';
+import Licorne from "../../assets/Licorne.png";
+import Dinosaure from "../../assets/Dinosaure.png";
+import Dragon from "../../assets/Dragon.png";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
@@ -17,19 +17,19 @@ const newToken = JSON.parse(token);
 const categories = [
   {
     name: "Licorne",
-    //image: Licorne,
+    image: Licorne,
     description:
       "Il/Elle aime galoper à travers les champs ouverts et les forêts, explorer de nouveaux endroits et rencontrer de nouvelles créatures. Il/elle aime aussi utiliser sa créativité pour forger de merveilleux souvenirs. Pendant notre temps libre, il/elle aime se poser, méditer et mettre en pratique ses acquis. Dans l'ensemble, il/elle est un ami et un compagnon merveilleux, toujours prêt à égayer la journée de n'importe qui avec sa nature enjouée et aimante.",
   },
   {
     name: "Dinosaure",
-    //image: Dinosaure,
+    image: Dinosaure,
     description:
       "Il/elle est courageux et déterminé, toujours prêt à protéger ses proches et à défendre ses convictions. Il/elle est incroyablement loyal et affectueux envers ceux en qui il a confiance, mais il/elle peut aussi être féroce et intimidant pour les étrangers. Il/elle est aussi incroyablement curieux et adore explorer son environnement, toujours à la recherche de nouvelles images et de nouveaux sons. Il/elle aime chasser pour jouer, explorer de nouveaux territoires et faire de longues promenades dans la nature. Il/elle aime aussi passer du temps avec sa famille et ses amis, et on le trouve souvent en train de jouer avec d'autres dinosaures. Pendant son temps libre, il/elle aime se prélasser au soleil et faire de longues siestes. Dans l'ensemble, il/elle est un compagnon loyal et féroce, toujours prêt à défendre ses proches et à explorer le monde qui l'entoure.",
   },
   {
     name: "Dragon",
-    //image: Dragon,
+    image: Dragon,
     description:
       "Il/elle est très fier et confiant, toujours avide d'aventures et de nouveaux défis. Il/elle est incroyablement intelligent et ingénieux, capable de réfléchir rapidement et de trouver des solutions créatives aux problèmes. Il/elle peut parfois être un peu têtu, mais il/elle est aussi farouchement loyal envers ceux qu'il/elle considère comme ses amis. Il/elle aime courir, jouer et amasser des trésors. Il/elle aime aussi utiliser son intelligence pour résoudre des puzzles et des énigmes, et défie souvent les autres dans des épreuves d'esprit et d'habileté. Pendant son temps libre, il/elle aime se prélasser au soleil et aiguiser ses griffes. Dans l'ensemble, il/elle est un compagnon redoutable et formidable, toujours prêt à aider ses amis ou dans une quête audacieuse.",
   },
@@ -44,23 +44,8 @@ const SigninForm = () => {
   const [phone, setPhone] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-
   const [category, setCategory] = useState("");
-
   const [message, setMessage] = useState("");
-
-  const handleEmailSubmit = (event) => {
-    setEmail(event.target.value);
-  };
-  const handlePhoneSubmit = (event) => {
-    setPhone(event.target.value);
-  };
-  const handlePasswordSubmit = (event) => {
-    setPassword(event.target.value);
-  };
-  const handleConfirmPasswordSubmit = (event) => {
-    setConfirmPassword(event.target.value);
-  };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -93,7 +78,9 @@ const SigninForm = () => {
       email: `${email}`,
       password: `${password}`,
       phone: `${phone}`,
+      category: `${category}`,
     };
+
     try {
       const response = await axios.post(`${baseUrl}/register`, newUser, {
         headers: { Authorization: `Bearer ${newToken}` },
@@ -113,7 +100,7 @@ const SigninForm = () => {
       </Helmet>
       <div className="input-container">
         <h1 className="title">Inscription</h1>
-        {message != "" && <p>{message}</p>}
+        {message !== "" && <p>{message}</p>}
         <form onSubmit={handleSubmit}>
           <input
             type="text"
@@ -134,30 +121,29 @@ const SigninForm = () => {
             placeholder="E-mail"
             name="email"
             value={email}
-            onChange={handleEmailSubmit}
+            onChange={(event) => setEmail(event.target.value)}
           />
           <input
             type="text"
             placeholder="Numéro de téléphone"
             name="phone"
             value={phone}
-            onChange={handlePhoneSubmit}
+            onChange={(event) => setPhone(event.target.value)}
           />
           <input
             type="password"
             placeholder="Mot de passe"
             name="password"
             value={password}
-            onChange={handlePasswordSubmit}
+            onChange={(event) => setPassword(event.target.value)}
           />
           <input
             type="password"
             placeholder="Validation mot de passe"
             name="confirmPassword"
             value={confirmPassword}
-            onChange={handleConfirmPasswordSubmit}
+            onChange={(event) => setConfirmPassword(event.target.value)}
           />
-
           <div className="categories">
             {categories.map((c) => (
               <div
@@ -183,10 +169,9 @@ const SigninForm = () => {
               </div>
             ))}
           </div>
-
-          <p className="validation" onClick={handleSubmit}>
-            <span>Valider</span>
-          </p>
+          <button type="submit" onClick={handleSubmit}>
+            Valider
+          </button>
         </form>
       </div>
     </>
